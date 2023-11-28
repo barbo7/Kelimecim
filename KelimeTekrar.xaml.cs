43eefaee18a55ad7ed2e371d0e9f9912ddc37ba6
barbo7 <b.sapancil@gmail.com>
2023-11-28 03:34:02 +0300

    New page and futures added

--- KelimeTekrar.xaml.cs
+++ KelimeTekrar.xaml.cs
@@ -2,8 +2,51 @@ namespace Kelimecim;
 
 public partial class KelimeTekrar : ContentPage
 {
-	public KelimeTekrar()
+    GoogleSheets gs = GoogleSheets.Instance;
+    CancellationTokenSource cancelTokenSource;
+    Tuple<string, string> veri;
+    public KelimeTekrar()
 	{
 		InitializeComponent();
-	}
+        YeniKelime();
+    }
+    private void SagButton_Clicked(object sender, EventArgs e)
+    {
+        if (!gs.kelimeSayfasiHazirMi)
+        {
+            DisplayAlert("Uyarý", "Sayfa henüz hazýr deðil tekrar deneyin", "Tamam");
+            return;
+        }
+
+        YeniKelime();
+    }
+    private void YeniKelime()
+    {
+        veri = gs.RastgeleKelimeGetirVTOrMyList(false);
+
+        wordText.Text = veri.Item2;
+        kelimeText.Text = veri.Item1;
+        MetindenSese(wordText.Text);
+    }
+
+    private async void MetindenSese(string textBoxText)
+    {
+        // Ýptal belirteci (CancellationToken) oluþtur
+        if (cancelTokenSource != null)
+        {
+            // Önceki okuma iþlemini iptal et
+            cancelTokenSource.Cancel();
+        }
+
+        // Yeni bir iptal belirteci oluþtur
+        cancelTokenSource = new CancellationTokenSource();
+
+        await TextToSpeech.SpeakAsync(textBoxText, cancelTokenSource.Token);
+    }
+
+    private void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        if (wordText != null && wordText.Text != null)
+            MetindenSese(wordText.Text);
+    }
 }
